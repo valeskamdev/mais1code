2968b97bf91c55b992d1b6164ad8532ac6215360
valeskamdev <valeskkitty@gmail.com>
2023-01-20 15:38:19 -0300

    1 sheep... 2 sheep

---
+++ facil/sheep.js
@@ -0,0 +1,8 @@
+function countSheep(number) {
+    for (var i = 1; i <= number; i++) {
+         console.log(`${i} carneirinho...`); 
+    }
+}
+
+countSheep(3); // 1 carneirinho...2 carneirinho...3 carneirinho...
+countSheep(1); // 1 carneirinho...
