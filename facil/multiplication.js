dc330bed7be2e50084ecf5633ba8776417bb495e
valeskamdev <valeskkitty@gmail.com>
2023-01-20 16:38:50 -0300

    Multiplication table

---
+++ facil/multiplication.js
@@ -0,0 +1,9 @@
+function multiplicationTable(number) {
+    for (var i = 1; i <= 10; i++) {
+        let result = number * i;
+        console.log(`${number} x ${i} = ${result}`)
+    }
+}
+
+multiplicationTable(3) // tabuada do 3 do 1 ao 10
+multiplicationTable(7) // tabuada do 7 do 1 ao 10
