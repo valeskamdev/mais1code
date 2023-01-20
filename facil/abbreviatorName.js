8d459735895da2ab7f90c35ae014e86394427de0
valeskamdev <107771511+valeskamdev@users.noreply.github.com>
2023-01-19 22:49:54 -0300

    abbreviate a compound name

--- facil/abbreviatorName.js
+++
@@ -1,7 +0,0 @@
-function abbreviateName(name) {
-    let initials = name.split(" ").map(function(n){ return n[0].toUpperCase()});
-    return initials.join(".")
-}
-
-console.log(abbreviateName("Flora Walker")); //F.W
-console.log(abbreviateName("Gabriel da Silva")); //G.D.S
