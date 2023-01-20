27ebfbba1117ad1f94afab766dd60db062ad55b6
valeskamdev <valeskkitty@gmail.com>
2023-01-20 15:59:52 -0300

    divisible number

---
+++ facil/divisible.js
@@ -0,0 +1,10 @@
+function divisibleNumber(x, y, z) {
+    if(x % y == 0 && x % z == 0) {
+        return true;
+    }
+    return false;
+}
+
+console.log(divisibleNumber(3, 1, 3)); //true
+console.log(divisibleNumber(100, 5, 3)); //false
+
