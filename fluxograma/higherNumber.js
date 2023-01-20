be6ad439bbe7938256cc4ed4a7873ef455612b01
valeskamdev <valeskkitty@gmail.com>
2023-01-20 17:07:00 -0300

    Higher number

---
+++ fluxograma/higherNumber.js
@@ -0,0 +1,6 @@
+function higherNumber(num1, num2, num3) {
+    return Math.max(num1, num2, num3);
+}
+    
+console.log(higherNumber(9, 23, 2)); // 23
+console.log(higherNumber(102, 98, 244)); //244
