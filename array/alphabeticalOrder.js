a00f76b8212ea532525297a64e9e05559de82b1f
valeskamdev <valeskkitty@gmail.com>
2023-01-20 18:31:16 -0300

    Alphabetical order

---
+++ array/alphabeticalOrder.js
@@ -0,0 +1,6 @@
+function alphabeticalOrder(words) {
+    return words.sort();
+}
+
+console.log(alphabeticalOrder(["Zebra", "Lontra", "Peixe", "Gato"])); // [ 'Gato', 'Lontra', 'Peixe', 'Zebra' ]
+console.log(alphabeticalOrder(["Laranja", "Amora", "Banana", "Jaca"])); // [ 'Amora', 'Banana', 'Jaca', 'Laranja' ]
