47576620155a922af338afb4ffc62849d1ebebec
valeskamdev <valeskkitty@gmail.com>
2023-01-22 13:44:42 -0300

    higherNumber

--- fluxograma/higherNumber.js
+++ fluxograma/higherNumber.js
@@ -1,5 +1,11 @@
 const higherNumber = (num1, num2, num3) => {
-    return Math.max(num1, num2, num3);
+   if (num1 > num2 && num1 > num3) {
+    return `O maior número é ${num1}`
+   } else if (num2 > num1 && num2 > num3) {
+    return `O maior número é ${num2}`
+   } else {
+    return `O maior número é ${num3}`
+   }
 }
     
 console.log(higherNumber(9, 23, 2)); // 23
